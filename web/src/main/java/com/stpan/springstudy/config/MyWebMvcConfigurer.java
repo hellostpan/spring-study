93a1dbee54417465a2763635a721c09f85ec8bec
stpan <852086659@qq.com.com>
2018-04-12 22:08:11 +0800

    has configurer error

--- web/src/main/java/com/stpan/springstudy/config/MyWebMvcConfigurer.java
+++ web/src/main/java/com/stpan/springstudy/config/MyWebMvcConfigurer.java
@@ -1,31 +1,110 @@
 package com.stpan.springstudy.config;
 
 
-import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
-import org.springframework.boot.web.servlet.filter.OrderedHiddenHttpMethodFilter;
-import org.springframework.boot.web.servlet.filter.OrderedHttpPutFormContentFilter;
+import com.stpan.springstudy.intercepter.OneInterceptor;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.validation.MessageCodesResolver;
+import org.springframework.validation.Validator;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.config.annotation.*;
+
+import java.util.List;
 
 /**
  * author:  STPAN
  * time:    2018/4/12 19:47
  */
-@Configuration
-public class MyWebMvcConfigurer extends WebMvcAutoConfiguration{
-    public MyWebMvcConfigurer() {
-        super();
-    }
-
-    @Override
-    public OrderedHiddenHttpMethodFilter hiddenHttpMethodFilter() {
-        return super.hiddenHttpMethodFilter();
-    }
+@EnableAutoConfiguration
+public class MyWebMvcConfigurer implements WebMvcConfigurer {
 
 
     @Override
-    public OrderedHttpPutFormContentFilter httpPutFormContentFilter() {
-        return super.httpPutFormContentFilter();
+    public void addFormatters(FormatterRegistry formatterRegistry) {
+
     }
 
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> list) {
 
+    }
+
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> list) {
+
+    }
+
+    @Override
+    public Validator getValidator() {
+        return null;
+    }
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer contentNegotiationConfigurer) {
+
+    }
+
+    @Override
+    public void configureAsyncSupport(AsyncSupportConfigurer asyncSupportConfigurer) {
+
+    }
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer pathMatchConfigurer) {
+
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> list) {
+
+    }
+
+    @Override
+    public void addReturnValueHandlers(List<HandlerMethodReturnValueHandler> list) {
+
+    }
+
+    @Override
+    public void configureHandlerExceptionResolvers(List<HandlerExceptionResolver> list) {
+
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry interceptorRegistry) {
+        interceptorRegistry.addInterceptor(new OneInterceptor());
+    }
+
+    @Override
+    public MessageCodesResolver getMessageCodesResolver() {
+        return null;
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry viewControllerRegistry) {
+
+    }
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry viewResolverRegistry) {
+
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry resourceHandlerRegistry) {
+
+    }
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer defaultServletHandlerConfigurer) {
+
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+
+    }
 }
